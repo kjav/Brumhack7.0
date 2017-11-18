d2b568b2d344328c5f3cc322f55439ac6184f632
dnadan2 <_dan_@live.com>
2017-11-18 20:38:27 +0000

    inventory part done

--- scripts/HudButtonClick.gd
+++ scripts/HudButtonClick.gd
@@ -5,4 +5,5 @@ const InventoryType = preload("res://Inventory.tscn")
 func _pressed():
 	self.get_parent().frameStyle
 	var new_inventory_instance = InventoryType.instance()
-	#get_tree().get_current_scene().add_child()
+	new_inventory_instance.set_pos(Vector2(97, 245))
+	get_tree().get_current_scene().add_child(new_inventory_instance)
