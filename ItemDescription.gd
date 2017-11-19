363ec908b2ef37187f8853d479e166020da5b5dd
dnadan2 <_dan_@live.com>
2017-11-19 03:11:47 +0000

    finally finished inventory system for all 3, and put in 3 food objects and descriptions, 3 spell and 3 potion

--- ItemDescription.gd
+++ ItemDescription.gd
@@ -1,27 +1,29 @@
 extends Node2D
 
-export(int, "HealthPot", "StrengthPot", "SpeedPots, FireSpell, GrabSpell, FreezeSpell, CookedSteak, CookedChicken, Cheese") var type setget setType, getType
+export(int, "HealthPot", "StrengthPot", "SpeedPots", "FireSpell", "GrabSpell", "FreezeSpell", "CookedSteak", "CookedChicken", "Cheese") var type setget setType, getType
 
 func getType():
 	return type
 
 func setType(newType):
-	if typeof(type) == typeof(0):
-		if type == 0:
+	type = newType
+	if typeof(type) == TYPE_STRING:
+		get_node("Name").set_text(type)
+		if type == "HealthPot":
 			get_node("Description").add_text("This drink heal your wounds!")
-		elif type == 1:
+		elif type == "StrengthPot":
 			get_node("Description").add_text("This drink give you newfound strength!")
-		elif type == 2:
+		elif type == "SpeedPot":
 			get_node("Description").add_text("This drink increases your reaction speed!")
-		elif type == 3:
+		elif type == "FireSpell":
 			get_node("Description").add_text("Shoot a fire ball at the closest enemy!")
-		elif type == 4:
+		elif type == "GrabSpell":
 			get_node("Description").add_text("Collect the closest item to you!")
-		elif type == 5:
+		elif type == "FreezeSpell":
 			get_node("Description").add_text("Stop the closest enemy for a turn!")
-		elif type == 6:
+		elif type == "CookedSteak":
 			get_node("Description").add_text("Mmmh suculent steak...")
-		elif type == 7:
-			get_node("Description").add_text("Chicken? down here... Don't complain!")
-		elif type == 8:
+		elif type == "CookedChicken":
+			get_node("Description").add_text("Chicken... down here? Don't complain!")
+		elif type == "Cheese":
 			get_node("Description").add_text("The cheese still kind of looks in date...")
--- scripts/HudButton.gd
+++ scripts/HudButton.gd
@@ -1,8 +1,8 @@
 tool
 extends Node2D
 
-export(int, "Right", "Centre", "Left") var frameStyle setget setFrameStyle, getFrameStyle
-export(int, "Potion", "Food", "Spell") var type setget setType, getType
+export(String, "Right", "Centre", "Left") var frameStyle setget setFrameStyle, getFrameStyle
+export(String, "Potion", "Food", "Spell") var type setget setType, getType
 
 var leftFrame = preload("res://assets//frame_left.png")
 var middleFrame = preload("res://assets//frame_none.png")
@@ -11,17 +11,14 @@ var potion = preload("res://assets//potion_inventory.png")
 var food = preload("res://assets//food_inventory2.png")
 var spell = preload("res://assets//book1.png")
 
-func _ready():
-	pass
-
 func setFrameStyle(style):
-	if typeof(style) == typeof(0):
+	if typeof(style) == TYPE_STRING:
 		frameStyle = style
-		if frameStyle == 2:
+		if frameStyle == "Left":
 			get_node("Background").set_texture(leftFrame)
-		elif frameStyle == 1:
+		elif frameStyle == "Centre":
 			get_node("Background").set_texture(middleFrame)
-		elif frameStyle == 0:
+		elif frameStyle == "Right":
 			get_node("Background").set_texture(rightFrame)
 
 func getFrameStyle():
@@ -31,11 +28,11 @@ func getType():
 	return type
 
 func setType(newType):
-	if typeof(newType) == typeof(0):
+	if typeof(newType) == TYPE_STRING:
 		type = newType
-		if type == 0:
+		if type == "Potion":
 			get_node("Icon").set_texture(potion)
-		elif type == 1:
+		elif type == "Food":
 			get_node("Icon").set_texture(food)
-		elif type == 2:
+		elif type == "Spell":
 			get_node("Icon").set_texture(spell)
