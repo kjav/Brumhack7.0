2bcdf745450eb1edca28eb6c899657160328806d
Sam <sam.rr.lee@gmail.com>
2018-04-13 18:40:37 +0100

    Fixed moon, added fireball animation

---
+++ Characters/scripts/Missile.gd
@@ -0,0 +1,42 @@
+extends Sprite
+
+export(NodePath) var target setget setTarget, getTarget
+export(float) var speed = 25
+export(float) var damage = 0
+
+var total_time = 0
+var delay = 0.05
+
+func _ready():
+	if target:
+		set_process(true)
+
+func init(_target, _texture, _pos, _speed, _damage):
+	set_texture(_texture)
+	set_pos(_pos)
+	speed = _speed
+	damage = _damage
+	target = _target
+
+func _process(delta):
+	total_time += delta
+	if total_time > delay:
+		if !is_visible():
+			self.show()
+		var half_tile = Vector2(64, 64)
+		var target_pos = get_node(target).get_pos() + half_tile
+		if get_pos().distance_squared_to(target_pos) <= speed * speed:
+			set_process(false)
+			get_node(target).takeDamage(damage)
+			get_parent().remove_child(self)
+			queue_free()
+		else:
+			set_rot((get_pos()).angle_to_point(target_pos) + 90)
+			set_pos(get_pos() + (target_pos - get_pos()).normalized() * speed)
+
+func setTarget(_target):
+	target = _target
+	set_process(true)
+
+func getTarget():
+	return target
--- Items/scripts/Spells.gd
+++ Items/scripts/Spells.gd
@@ -1,3 +1,6 @@
+const missile = preload("res://Characters/Missile.tscn")
+const missile_texture = preload("res://assets/fireball.png")
+
 class FireSpell extends "Item.gd":
 	const texture = preload("res://assets/red_spell2.png")
 	
@@ -5,8 +8,20 @@ class FireSpell extends "Item.gd":
 		iconFilePath = "res://assets/red_spell2.png"
 		description = "Shoot a fire ball at the closest enemy!"
 		name = "Fire Spell"
-	
+
 	func onUse():
-		GameData.closestEnemy().takeDamage(1)
-		#animate a fireball
-		GameData.spells.remove(GameData.spells.find(self))
+		var closest_enemy = GameData.closestEnemy()
+		if closest_enemy:
+			# Remove potion
+			GameData.spells.remove(GameData.spells.find(self))
+			
+			# Launch a fireball
+			var new_missile = missile.instance()
+			GameData.player.get_parent().add_child(new_missile)
+			new_missile.init(
+				closest_enemy.get_path(),
+				missile_texture,
+				GameData.player.get_pos(),
+				25,
+				10
+			)
