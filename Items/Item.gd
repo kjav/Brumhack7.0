d9ea763843d13006c508482ef78367a47e789f53
dnadan2 <_dan_@live.com>
2018-04-13 16:47:40 +0100

    added in functionality to place food, spell, potion, weapon on map floor and pick them up/ drop them

--- Items/Item.gd
+++ Items/Item.gd
@@ -3,6 +3,13 @@ extends Node
 var description
 var name
 var iconFilePath
+var pos = null
 
 func onUse():
 	pass
+
+func place(newPos):
+	pos = newPos
+
+func pickup():
+	pos = null
--- Items/Spells.gd
+++ Items/Spells.gd
@@ -10,3 +10,8 @@ class FireSpell extends "Item.gd":
 		GameData.closestEnemy().takeDamage(1)
 		#animate a fireball
 		GameData.spells.remove(GameData.spells.find(self))
+
+func pickup():
+	#todo, needs to check if inventory is full first
+	GameData.addSpells([self])
+	.pickup()
