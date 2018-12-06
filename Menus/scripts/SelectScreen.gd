0be24cd5f4938f41a3376dd8d72b79ca73774cf5
dnadan2 <_dan_@live.com>
2018-12-06 21:45:46 +0000

    refactored select screen

--- Menus/scripts/SelectScreen.gd
+++ Menus/scripts/SelectScreen.gd
@@ -12,6 +12,7 @@ func _ready():
 	items = self.get_parent().get_items()
 	left_arrow = get_node("LeftArrow")
 	right_arrow = get_node("RightArrow")
+	
 	if items.size() > 0:
 		select_item(0)
 
@@ -25,16 +26,32 @@ func right():
 
 func select_item(item):
 	current_item = item
-	for i in range(items.size()):
-		items[i].set_pos(Vector2(10000, 10000))
-	items[current_item].set_pos(Vector2(540, 550))
 	
-	# Disable arrows if they can't be used
-	var leftmost = current_item == 0
-	var rightmost = current_item == items.size() - 1
-	left_arrow.set_opacity(1 - 0.8 * leftmost)
-	right_arrow.set_opacity(1 - 0.8 * rightmost)
+	hideAllButCurrent()
+	
+	DisableUnusableArrows()
+	
 	emit_signal("changed", current_item)
+
+func hideAllButCurrent():
+	for i in range(items.size()):
+		items[i].hide()
+	
+	items[current_item].set_pos(Vector2(540, 550))
+	items[current_item].show()
+
+func DisableUnusableArrows():
+	var leftArrowOpacity = 1
+	var rightArrowOpacity = current_item == items.size() - 1
+	
+	if current_item == 0:
+		leftArrowOpacity = 0.2
+	
+	if current_item == items.size() - 1:
+		rightArrowOpacity = 0.2
+	
+	left_arrow.set_opacity(leftArrowOpacity)
+	right_arrow.set_opacity(rightArrowOpacity)
 
 func selected_item():
 	return items[current_item]
